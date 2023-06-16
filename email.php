891c01ccd653054c2c50babfeaa8b3d41d4fd405
iamkk5 <134827697+iamkk5@users.noreply.github.com>
2023-06-16 16:39:27 +0630

    Update email.php

--- email.php
+++ email.php
@@ -1,12 +1,3 @@
 <?php 
 echo "sending email...";
 echo "Another email sent!";
-
-echo "Third line from Cli";
-echo "Third line from online";
-
-echo "Forth line from cli";
-echo "Forth line from online";
-
-echo "Fifth line from online";
-echo "Fifth line from cli";
